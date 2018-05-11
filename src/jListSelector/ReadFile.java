3928edd96c7a0408f2ff6d204addf4297aa32014
Thomas Pumfrey <thomaspumfrey@gmail.com>
2018-05-11 11:36:46 +0100

    ReadFile class
    Class to read text file

---
+++ src/jListSelector/ReadFile.java
@@ -0,0 +1,79 @@
+
+package jListSelector;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+import java.util.ArrayList;
+
+
+public class ReadFile {
+    
+    private String path;
+    
+    
+    
+    int readLines() throws IOException {
+
+        FileReader file_to_read = new FileReader (path);
+        BufferedReader bf = new BufferedReader(file_to_read);
+
+        String aLine;
+        int numberOfLines = 0;
+
+        while (( aLine = bf.readLine()) != null) {
+        numberOfLines++;
+        }
+        bf.close();
+
+        return numberOfLines;
+    }
+    
+    
+    
+    
+    public ReadFile (String file_path) {
+        path = file_path;
+    }
+    
+        public ArrayList OpenFile() throws IOException {
+        
+        FileReader fr = new FileReader (path);
+        BufferedReader textReader = new BufferedReader (fr);
+        
+        int numberOfLines = readLines();
+        ArrayList textData = new ArrayList();
+        
+        int i;
+        
+        for (i = 0; i < numberOfLines; i++) {
+            textData.add( textReader.readLine());
+        }
+        
+        textReader.close();
+        return textData;
+    }
+}
+    
+    
+    
+//    public String[] OpenFile() throws IOException {
+//        
+//        FileReader fr = new FileReader (path);
+//        BufferedReader textReader = new BufferedReader (fr);
+//        
+//        int numberOfLines = readLines();
+//        String[] textData = new String[numberOfLines];
+//        
+//        int i;
+//        
+//        for (i = 0; i < numberOfLines; i++) {
+//            textData[i] = textReader.readLine();
+//        }
+//        
+//        textReader.close();
+//        return textData;
+//    }
+//}
+
