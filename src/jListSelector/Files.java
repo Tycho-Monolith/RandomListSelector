57e4226277d10df52d94aa6969a9e14c7bf0f0a8
Tycho-Monolith <38956835+Tycho-Monolith@users.noreply.github.com>
2018-05-23 11:05:37 +0100

    FileLocator
    Tidied up the code and removed unused methods

--- src/jListSelector/Files.java
+++ src/jListSelector/Files.java
@@ -2,33 +2,26 @@
 package jListSelector;
 
 import java.io.File;
-//import org.apache.commons.io.FilenameUtils;
 
 
 public class Files {
     
-//    private File File_Location;
-//    private File[] List_Of_Files;
-    
     public String locationOfSaveFiles = "C:/Users/Tom/Documents/Java Projects/RandomListSelector/List Saves";
-    public File folder = new File("C:/Users/Tom/Documents/Java Projects/RandomListSelector/List Saves");
+    
+    public File folder = new File(locationOfSaveFiles);
     public File[] listOfFiles = folder.listFiles();
 
     
-    
+    //returns the number of lists (i.e. txt files) in the folder
     int numberOfLists() {
+        
         int numberOfLists = listOfFiles.length;
         return numberOfLists;
-    }
-        
-    //returns the file location from the list/file name
-    String fileLocationFromFileName(String fileName) {
-        
-        String location = locationOfSaveFiles + "/" + fileName + ".txt";
-        return location;
         
     }
     
+   
+    //returns the name of the list from an integer (to be used in a loop)
     String findFiles(int i) {
         
         String nameOfFile = listOfFiles[i].getName();
@@ -36,67 +29,32 @@ public class Files {
         return nameOfList;
     }
     
-    //returns the file location from a 
-    String fileLocation(String fileName) {
+    
+    //returns the file location from a list name
+    String fileLocation(String listName) {
         
         String filePath = "";
             
         for (int i = 0; i < numberOfLists(); i++) {
             
-            if (fileName.equals(findFiles(i))) {
+            if (listName.equals(findFiles(i))) {
                 filePath = listOfFiles[i].getPath();
-            }  
-            
+            }     
         }
         return filePath;
-        
-    } 
- 
-    
-    void printListNames() {
-        
-        int i;
-        for (i = 0; i < listOfFiles.length; i++) {
-            String fileName = findFiles(i);
-            System.out.println(fileName);
-             
-            
-//            String nameOfFile = String.valueOf(listOfFiles[i]);
-//            String nameOfList = removeExtension(nameOfFile);
-//            System.out.println(nameOfList);
-        }
-    }
+    }    
     
     
-    
+    //removes the ".txt" extension from a file name
     String removeExtension(String nameOfFile) {
         
-        String newNameOfFile = "";
+        String nameOfList = "";
         if (nameOfFile.indexOf(".txt") > 0) {
-            newNameOfFile = nameOfFile.substring(0, nameOfFile.lastIndexOf(".txt"));
+            nameOfList = nameOfFile.substring(0, nameOfFile.lastIndexOf(".txt"));
         }
-        return newNameOfFile;
+        return nameOfList;
         
-    }
-    
+    }  
 
     
-    
-//    void findFiles() {
-//    
-//        int i;
-//        for (i = 0; i < listOfFiles.length; i++) {
-//            if (listOfFiles[i].isFile()) {
-//                System.out.println("File " + listOfFiles[i].getName());
-//            }
-//            else if (listOfFiles[i].isDirectory()) {
-//                System.out.println("Directory " + listOfFiles[i].getName());
-//            }
-//        }    
-//    }
-    
-
-        
-}    
-    
-
+}
